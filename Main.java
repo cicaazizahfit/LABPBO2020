c1551f94ce69e256326ac60862a7785fb115af51
cicaazizahfit <49094284+cicaazizahfit@users.noreply.github.com>
2020-04-19 17:11:46 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,66 @@
 public class Main {
-    public static void main (String[] args){
-        int defense = 15;
-        int attack = 30;
-        Player player1 = new Player ("Mino", attack, defense);
-        Player player2 = new Player ("Hilda", defense);
+    public static void main(String[] args) {
+        ConverterData cData = new ConverterData();
+        cData.start();
+        if (cData.teraToGiga(9) == 9216) {
+            System.out.println("Terabyte To Gigabyte = OK");
+            if (cData.teraToMega(16) == 16777216) {
+                System.out.println("Terabyte To Megabyte = OK");
+                if (cData.teraToKilo(1) == 1073741824) {
+                    System.out.println("Terabyte To Kilobyte = OK");
+                } else {
+                    System.out.println("Terabyte To Kilobyte = INCORRECT");
+                    return;
+                }
+            } else {
+                System.out.println("Terabyte To Megabyte = INCORRECT");
+                return;
+            }
+        } else {
+            System.out.println("Terabyte To Gigabyte = INCORRECT");
+            return;
+        }
+        cData.stop();
+        System.out.println("\n");
 
-        
-        //lengkapi
-        player2.setAttackPower(35);
-        
-        player1.getDamage(player2);
-        player2.getDamage(player1);
-        player1.status();
-        player2.status();
+        ConverterTemperature cTemp = new ConverterTemperature();
+        cTemp.start();
+        if (cTemp.celciusToFahrenheit(9) == 48.2) {
+            System.out.println("Celcius to Fahrenheit = OK");
+            if (cTemp.celciusToKelvin(11) == 284.15) {
+                System.out.println("Celcius to Kelvin = OK");
+            } else {
+                System.out.println("Celcius to Kelvin = INCORRECT");
+                return;
+            }
+        } else {
+            System.out.println("Celcius to Fahrenheit = INCORRECT");
+            return;
+        }
+        cTemp.stop();
+        System.out.println("\n");
+
+        ConverterDistance cDistance = new ConverterDistance();
+        cDistance.start();
+        if (cDistance.inchToMetre(1) == 0.0254) {
+            System.out.println("Inch to Metre = OK");
+            if (cDistance.metreToKilo(12500) == 12.5) {
+                System.out.println("Metre to Kilometre = OK");
+                if (cDistance.metreToMilli(12.9) == 12900) {
+                    System.out.println("Metre to Millimetre = OK");
+                } else {
+                    System.out.println("Metre to Millimetre = INCORRECT");
+                    return;
+                }
+            } else {
+                System.out.println("Metre to Kilometre = INCORRECT");
+                return;
+            }
+        } else {
+            System.out.println("Inch to Metre = INCORRECT");
+            return;
+        }
+        cDistance.stop();
+        System.out.println("\n");
     }
 }
---
+++ Temperature.java
@@ -0,0 +1,10 @@
+public interface Temperature {
+    public double celciusToFahrenheit(double value);
+    public double celciusToKelvin(double value);
+}
+
+
+// https://prod.liveshare.vsengsaas.visualstudio.com/join?EEC3767965620E2530E14AB9144833AF091B
+
+
+// oiiii 
